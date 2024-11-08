1b0415286b6bcaf1b39d53cbae90a5852ec79164
Daniel Winning <DanielWinning@users.noreply.github.com>
2024-11-08 23:37:01 +0000

    Update login form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,22 +4,25 @@ namespace Luma\SecurityComponent\Controller;
 
 use Luma\Framework\Controller\LumaController;
 use Luma\Framework\Luma;
+use Luma\Framework\Messages\FlashMessage;
 use Luma\HttpComponent\Request;
 use Luma\HttpComponent\Response;
 use Luma\SecurityComponent\Form\LoginForm;
+use Luma\SecurityComponent\Session\DatabaseSessionManager;
 
 class SecurityController extends LumaController
 {
     private string $loginTemplate;
     private string $registrationTemplate;
-    private string $userClassString;
+    private string $userClass;
 
     public function __construct()
     {
         parent::__construct();
 
-        $this->loginTemplate = Luma::getConfigParam('security.loginTemplate');
-        $this->registrationTemplate = Luma::getConfigParam('security.registrationTemplate');
+        $this->loginTemplate = Luma::getConfigParam('security.loginTemplate') ?? '';
+        $this->registrationTemplate = Luma::getConfigParam('security.registrationTemplate') ?? '';
+        $this->userClass = Luma::getConfigParam('security.userClass') ?? '';
     }
 
     public function login(Request $request): Response
@@ -27,12 +30,41 @@ class SecurityController extends LumaController
         $form = new LoginForm([]);
 
         if ($request->getMethod() === 'POST') {
-            // Attempt to log in
             $form = new LoginForm($_POST);
+
+            if (!$request->get($this->userClass::getSecurityIdentifier()) || !$request->get('password')) {
+                $this->addFlashMessage(
+                    new FlashMessage(sprintf('Invalid request, please set %s and password', $this->userClass::getSecurityIdentifier())),
+                    FlashMessage::ERROR
+                );
+
+                $this->redirect('/login');
+            }
+
+            $loginResult = Luma::getAuthenticator()->authenticate(
+                $request->get($this->userClass::getSecurityIdentifier()),
+                $request->get('password')
+            );
+
+            if ($loginResult->isAuthenticated()) {
+                DatabaseSessionManager::regenerate();
+                DatabaseSessionManager::setSessionItem('user', $loginResult->getUser());
+
+                $this->addFlashMessage(
+                    new FlashMessage('Successfully logged in, welcome back.'),
+                    FlashMessage::SUCCESS
+                );
+
+                return $this->redirect('/');
+            } else {
+                $this->addFlashMessage(
+                    new FlashMessage('Invalid credentials, please check and try again.'),
+                    FlashMessage::ERROR
+                );
+            }
         }
 
-        // Render the login form
-        return $this->render(Luma::getConfigParam('security.loginTemplate') ?? '', [
+        return $this->render($this->loginTemplate, [
             'form' => $form,
         ]);
     }
